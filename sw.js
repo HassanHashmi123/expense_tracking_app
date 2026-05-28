b9bee9859dac3b9e369a8ade23e681a81478b2e1
HassanHashmi123 <hassanhashmi078@gmail.com>
2026-05-28 23:15:43 +0500

    Add PWA support

---
+++ sw.js
@@ -0,0 +1,59 @@
+const CACHE_NAME = 'expense-tracker-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './expense_tracking_app.html',
+    './manifest.json',
+    './icons/icon-192.svg',
+    './icons/icon-512.svg',
+    'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Cache opened');
+                return cache.addAll(urlsToCache);
+            })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+
+                return fetch(event.request)
+                    .then(networkResponse => {
+                        if (!networkResponse || networkResponse.status !== 200) {
+                            return networkResponse;
+                        }
+
+                        const responseToCache = networkResponse.clone();
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return networkResponse;
+                    })
+                    .catch(() => caches.match('./index.html'));
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => Promise.all(
+            cacheNames
+                .filter(name => name !== CACHE_NAME)
+                .map(name => caches.delete(name))
+        ))
+    );
+    self.clients.claim();
+});
